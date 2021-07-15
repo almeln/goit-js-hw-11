3b6af3210b9bd0dc92ee4f0e3ee7675a929ff240
Alina Melnyk <alinkaaa345@gmail.com>
2021-07-15 23:18:22 +0300

    добавляет промисы

--- src/js/03-promisify.js
+++ src/js/03-promisify.js
@@ -1,6 +1,11 @@
 // ======================= Subtask 1 =======================
 const delay = ms => {
   // Change this function
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(ms);
+    })
+  }, ms)
 };
 
 const logger = time => console.log(`Fulfilled after ${time}ms`);
@@ -10,6 +15,7 @@ delay(2000).then(logger); // Fulfilled after 2000ms
 delay(1000).then(logger); // Fulfilled after 1000ms
 delay(1500).then(logger); // Fulfilled after 1500ms
 
+
 // ======================= Subtask 2 =======================
 const users = [
   { name: 'Mango', active: true },
@@ -17,12 +23,14 @@ const users = [
   { name: 'Ajax', active: false },
 ];
 
-const toggleUserState = (allUsers, username, callback) => {
+const toggleUserState = (allUsers, username) => {
   const updatedUsers = allUsers.map(user =>
     user.name === username ? { ...user, active: !user.active } : user
   );
 
-  callback(updatedUsers);
+  return Promise.resolve(updatedUsers);
+
+  // callback(updatedUsers);
 };
 
 // Currently the function works like this
@@ -30,26 +38,29 @@ const toggleUserState = (allUsers, username, callback) => {
 // toggleUserState(users, 'Ajax', console.table);
 
 // The function should work like this
-// toggleUserState(users, 'Mango').then(console.table);
-// toggleUserState(users, 'Ajax').then(console.table);
+toggleUserState(users, 'Mango').then(console.table);
+toggleUserState(users, 'Ajax').then(console.table);
+
 
 // ======================= Subtask 3 =======================
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-const makeTransaction = (transaction, onSuccess, onError) => {
+const makeTransaction = (transaction) => {
   const delay = randomIntegerFromInterval(200, 500);
 
-  setTimeout(() => {
-    const canProcess = Math.random() > 0.3;
-
-    if (canProcess) {
-      onSuccess({ id: transaction.id, time: delay });
-    } else {
-      onError(transaction.id);
-    }
-  }, delay);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const canProcess = Math.random() > 0.3;
+  
+      if (canProcess) {
+        resolve({ id: transaction.id, time: delay });
+      } else {
+        reject(transaction.id);
+      }
+    }, delay);
+  })
 };
 
 const logSuccess = ({ id, time }) => {
@@ -67,7 +78,7 @@ const logError = id => {
 // makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
 
 // The function should work like this
-// makeTransaction({ id: 70, amount: 150 }).then(logSuccess).catch(logError);
-// makeTransaction({ id: 71, amount: 230 }).then(logSuccess).catch(logError);
-// makeTransaction({ id: 72, amount: 75 }).then(logSuccess).catch(logError);
-// makeTransaction({ id: 73, amount: 100 }).then(logSuccess).catch(logError);
+makeTransaction({ id: 70, amount: 150 }).then(logSuccess).catch(logError);
+makeTransaction({ id: 71, amount: 230 }).then(logSuccess).catch(logError);
+makeTransaction({ id: 72, amount: 75 }).then(logSuccess).catch(logError);
+makeTransaction({ id: 73, amount: 100 }).then(logSuccess).catch(logError);
