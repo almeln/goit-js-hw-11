aff4386ce72993341fb2777b13da98e3d5ed05ee
Alina Melnyk <alinkaaa345@gmail.com>
2021-07-16 20:02:38 +0300

    исправляет ошибку в промисе

--- src/js/03-promisify.js
+++ src/js/03-promisify.js
@@ -4,8 +4,8 @@ const delay = ms => {
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve(ms);
-    })
-  }, ms)
+    }, ms)
+  })
 };
 
 const logger = time => console.log(`Fulfilled after ${time}ms`);
